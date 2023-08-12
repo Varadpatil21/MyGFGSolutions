da180ec0994870bd44ef42f5cf6e6fe26fd80eca
Varadpatil21 <96022723+Varadpatil21@users.noreply.github.com>
2023-08-12 23:50:28 +0530

    Added Solution - GfG to GitHub

---
+++ School/Print 1 To N Without Loop/print-1-to-n-without-loop.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    void printNos(int N)
+    {
+        //Your code here
+        int index=1;
+        solve(N,index);
+        
+    }
+    void solve(int N,int i)
+    {
+        if(i>N)
+        return;
+        cout<<i<<" ";
+        solve(N,i+1);
+    }
+};
+
+//{ Driver Code Starts.
+/* Driver program to test printNos */
+int main()
+{
+    int T;
+    
+    //taking testcases
+    cin>>T;
+    
+    while(T--)
+    {
+        int N;
+        
+        //input N
+        cin>>N;
+        Solution ob;
+        //calling printNos() function
+        ob.printNos(N);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
